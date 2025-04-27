48f814aa8cd0aa5f3251546bc2407cf912e2132c
thabobosvark <thabobosvark@icloud.com>
2025-04-27 19:26:29 +0200

    Complete code.c and add Makefile

--- tutorial1/app/code.c
+++ tutorial1/app/code.c
@@ -21,11 +21,17 @@ void get_name(char* name)
 // 1. Function to create a folder using the name variable as its name
 void create_folder(char* name)
 {
-
+mkdir(name,0777)
 }
 
 // 2. Function to create a file in the folder <folder_name> called group.txt where you insert the <group_name> as text
 void create_file(char* folder_name, char* group_name)
 {
-
+char filename[100];
+snprintf(filename, sizeof(filename), "%s/%s.txt", folder_name, group_name);
+    FILE* file = fopen(filename, "w");
+    if (file != NULL) {
+        fprintf(file, "Group: %s\n", group_name);
+        fclose(file);
+    }
 }
